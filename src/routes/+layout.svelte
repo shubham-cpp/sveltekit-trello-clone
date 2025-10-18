2ab9ed8020f92355d74b8f73ce5ef9fcfbb5a6a4
Shubham Pawar <shubhampawar3007@gmail.com>
2025-10-18 10:02:01 +0530

    feat: shadcn-svelte setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	
+	import { ModeWatcher } from 'mode-watcher';
+	import '../app.css';
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<meta
+		name="description"
+		content="A Jira-Clone app build with SvelteKit, Better-Auth and Drizzle"
+	/>
+	<meta name="author" content="Shubham Pawar(https://github.com/shubham-cpp)" />
 </svelte:head>
-
+<ModeWatcher />
 {@render children?.()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script lang="ts">
+	import ThemeToggle from '$lib/components/theme-toggle.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
